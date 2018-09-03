053535964de9e828af1e4abe46532e6e0e5d85b6
oleg-hub <vbk80g@mail.ru>
2018-09-03 15:35:31 +0300

    ExampleforCMTSite

---
+++ WebApp111/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp111.Models;
+
+namespace WebApp111.Controllers
+{
+    public class HomeController : Controller
+    {
+        ModelModel m = new ModelModel();
+        public ActionResult Index()
+        {
+            return View(m);
+        }
+
+        public PartialViewResult PartialV()
+        {
+
+            //ModelModel m = new ModelModel();
+            //HttpPostedFileBase model = null;
+            //var length = model.InputStream.Length;
+            //byte[] fileData = null;
+            //using (var binaryReader = new BinaryReader(model.InputStream))
+            //{
+            //    fileData = binaryReader.ReadBytes(model.ContentLength);
+            //}
+            //m.TopBar = fileData;
+
+            return PartialView(m);
+        }
+
+        [HttpPost]
+        public ActionResult Index(/*int type,*/ HttpPostedFileBase model)
+        {
+
+            ModelModel m = new ModelModel();
+
+            if (model != null)
+            {
+                var length = model.InputStream.Length;
+                byte[] fileData = null;
+                using (var binaryReader = new BinaryReader(model.InputStream))
+                {
+                    fileData = binaryReader.ReadBytes(model.ContentLength);
+                }
+                m.TopBar = fileData;
+            }
+
+            return View(m);
+        }
+
+        [HttpPost]
+        
+        public ActionResult SaveUserSignature(int id, HttpPostedFileBase signatureFile,string file)
+        {
+            return Json(new { success = false});
+        }
+
+    }
+}
---
+++ WebApp111/Models/ModelModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp111.Models
+{
+    public class ModelModel
+    {
+        public int Id { get; set; }
+        public Dictionary<int, string> ThemeDevices { get; set; }
+        public byte[] Background { get; set; }
+        public byte[] TopBar { get; set; }
+        public byte[] BottomBar { get; set; }
+        public byte[] CenterOrbit { get; set; }
+        public byte[] NotifyButton { get; set; }
+        public byte[] TopButton { get; set; }
+        public byte[] LeftButton { get; set; }
+        public byte[] RightButton { get; set; }
+
+        public List<byte[]> AllImages;
+    }
+}
